8831369048eeb43a683187c0bd511895cae3bcc5
mikey <micjay13@gmail.com>
2020-07-30 19:30:46 +0000

    added dummy forms

--- index.php
+++ index.php
@@ -68,13 +68,32 @@ require './userHandler.php';
 </body>
 
 <div class="form_container2">
-    <h2>Add Volume</h2>
+    <h2>Add Report</h2>
     <form action="<?= $_SERVER["PHP_SELF"]; ?>" method="post">
-        <input type="text" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <input type="submit" value="Login" name="submit_login">
+        <input type="text" name="id" placeholder="VolumeID">
+        <input type="text" name="Report_id" placeholder="Report_id">
+        <input type="text" name="Report_title" placeholder="Report_title">
+        <input type="text" name="Report_summary" placeholder="Report_summary">
+        <input type="text" name="Report_highlights" placeholder="Report_highlights">
+        <input type="text" name="Report_this_issue" placeholder="Report_this_issue">
+        <input type="text" name="Report_date" placeholder="Report_date">
+        <input type="text" name="Report_description" placeholder="Report_description">
+        <input type="text" name="Report_authors" placeholder="Report_authors">
+        <input type="submit" value="Add" name="add_report">
     </form>
 </div>
+
+<div class="form_container2">
+    <h2>Add Volume</h2>
+    <form action="<?= $_SERVER["PHP_SELF"]; ?>" method="post">
+        <input type="text" name="Volume_id" placeholder="Volume_id">
+        <input type="text" name="Volume_title" placeholder="Volume_title">
+        <input type="text" name="Volume_year" placeholder="Volume_year">
+        <input type="text" name="Volume_description" placeholder="Volume_description">
+        <input type="submit" value="Add" name="add_volume">
+    </form>
+</div>
+
 </body>
 
 </html>
